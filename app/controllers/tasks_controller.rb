a048ed8f78a9ad96964ae309a1547806a3c5f6f9
TaisonTsukada <78004609+TaisonTsukada@users.noreply.github.com>
2021-06-04 17:23:08 +0900

    細かい微調整

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,65 +1,64 @@
 class TasksController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create, :destroy, :edit, :assign]
-    before_action :set_task, only: [:show, :destroy, :edit, :update, :assign]
-    before_action :move_to_index, only: [:edit, :update, :assign]
+  before_action :authenticate_user!, only: [:new, :create, :destroy, :edit, :assign]
+  before_action :set_task, only: [:show, :destroy, :edit, :update, :assign]
+  before_action :move_to_index, only: [:edit, :update, :assign]
 
-    def index
-        @tasks = Task.index_all.page(params[:page])
+  def index
+      @tasks = Task.index_all.page(params[:page])
+  end
+
+  def new
+      @task = Task.new
+  end
+
+  def show
+
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
     end
+  end
 
-    def new
-        @task = Task.new
+  def edit
+  end
+
+  def update
+      if @task.update(task_params)
+          redirect_to task_path
+      else
+          render :edit
+      end
+  end
+
+  def destroy
+    if current_user.id == @task.user.id
+      @task.destroy
+      redirect_to root_path
     end
+  end
 
-    def show
+  def assign
+  end
 
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content, :deadline, :status_id, :user_id)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def move_to_index
+    set_task
+    unless current_user.id ==  @task.user.id
+      redirect_to root_path
     end
-
-    def create
-        @task = Task.new(task_params)
-        if @task.save
-            redirect_to root_path
-        else
-            render :new
-        end
-    end
-
-    def edit
-    end
-
-    def update
-        if @task.update(task_params)
-            redirect_to task_path
-        else
-            render :edit
-        end
-    end
-
-    def destroy
-        if current_user.id == @task.user.id
-            @task.destroy
-            redirect_to root_path
-        end
-    end
-
-    def assign
-    end
-
-    private
-
-    def task_params
-        params.require(:task).permit(:title, :content, :deadline, :status_id, :user_id)
-    end
-
-    def set_task
-        @task = Task.find(params[:id])
-    end
-
-    def move_to_index
-        set_task
-        unless current_user.id ==  @task.user.id
-            redirect_to root_path
-        end
-    end
-
+  end
 end
